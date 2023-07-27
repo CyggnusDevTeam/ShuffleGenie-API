7d0fea30989063df1f82f31f6c210c0117f8f6ba
Leonardo Kotlarenko <lkotlarenko@proton.me>
2023-07-27 16:38:37 -0300

    refactor: ♻️format 'index.js'

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 functions.http('fetchData', async (req, res) => {
+  const DATA_DOMAIN = 'ENDPOINT URL FOR DATA COLLECTION GOES HERE';
+  const { user } = req.query;
+  
   res.set('Access-Control-Allow-Origin', '*');
   res.set('Access-Control-Allow-Methods', 'GET, POST');
 
-  const DATA_DOMAIN = 'ENDPOINT URL FOR DATA COLLECTION GOES HERE';
-
   if (req.method === 'OPTIONS') {
-    // stop preflight requests here
     res.status(204).send('Invalid request');
     return;
   }
 
-  const { user } = req.query;
   if (!user) {
     res.status(422).send('API Error: Missing params!');
     return;
@@ -24,17 +23,17 @@ functions.http('fetchData', async (req, res) => {
     try {
       const { data: html } = await axios.get(`${DATA_DOMAIN}users/${user}`);
       const $ = cheerio.load(html);
-
       const cards = [];
+      
       $('#collection .card-collection').each((_, card) => {
         const id = $(card).attr('data-cid');
         const name = $(card).find('.name').text();
         const imgUrl = $(card).find('img').attr('data-src');
-
         cards.push({ id, name, imgUrl });
       });
-
+      
       res.json(cards);
+      
     } catch (error) {
       res.status(500).send(`Failed to retrieve user collection - ${error}`);
     }
